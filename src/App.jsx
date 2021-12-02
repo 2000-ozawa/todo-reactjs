5f9aa59e5c5f4b85eb86c23105b7865604cfd053
Ozawa Ryo <92570622+2000-ozawa@users.noreply.github.com>
2021-12-02 14:26:13 +0900

    不要なjsx削除 初期値を削除して完成

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,8 @@ import "./styles.css";
 
 export const App = () => {
   const [todoText, setTodoText] = useState("");
-  const [incompleteTodos, setIncompleteTodos] = useState([
-    "ああああ",
-    "いいいいい"
-  ]);
-  const [completeTodos, setCompleteTodos] = useState(["うううううう"]);
+  const [incompleteTodos, setIncompleteTodos] = useState([]);
+  const [completeTodos, setCompleteTodos] = useState([]);
 
   const onChangeTodoText = (event) => setTodoText(event.target.value);
 
